6441ac2a30b85593cf37b98d5ef3286c578fee49
you <you@example.com>
2017-08-14 13:45:58 +0200

    Init modern with CMake

---
+++ src/sndwch.h
@@ -0,0 +1,99 @@
+#include "stddef.h"
+#include "strings.h"
+/* 
+ * 
+
+what do i want:
+
+
+i have some fig-h_min-h_max.svg files
+
+place at (x,y) and h_min, h_max
+
+
+-cut: /evefa/vadfa/
+  x:      57  
+  y:      10
+  z_min:  4
+  z_max:  6
+
+
+> cuts.yaml
+
+the library should:
+. read the input .yaml file
+. parse into list of 3Dcut objects
+. slice together the 3Dcut objects into a list of layers
+. merge together equal layers (MergeEqualLayers)
+. write the layers to file (WriteLayersToFile)
+
+
+. ext: write visual makes a svg file that shows them one above the other
+
+
+layers_t
+
+SliceByMM : 3dcuts to lauers
+
+MergeEqualLayers : given a list of layers, merges the same  
+
+writeLayersToFile
+
+writeVisual : write visual makes a svg file that shows them one above the other
+
+MakeSandwich : 
+
+
+
+ *
+ * make_sandwich(char *out_dir,  , );
+ *
+ *
+ */
+
+/* Config */
+static const size_t PATH_MAX = 256;
+static const size_t CUTS_MAX = 10;
+
+/* Custom error message type */
+typedef int snd_err_t;
+
+/* Custom error codes */
+static const snd_err_t SND_OK = 0;
+static const snd_err_t SND_ERR_NO_SPACE = 0;
+static const snd_err_t SND_O = 0;
+
+typedef struct {
+	char path[PATH_MAX];
+	float x;
+	float y;
+} cut_2d_t;
+
+typedef struct {
+	cut_2d_t cut;
+	float zmin;
+	float zmax;
+} cut_3d_t;
+
+typedef struct {
+	cut_2d_t cut[CUTS_MAX];
+	float zmin;
+	float zmax;
+} layer_t;
+
+/* reads all the SVG elements of a file into a buffer */
+snd_err_t importSVGElementsFromFile(const char *path, char *buf, size_t buf_len)
+{
+	return SND_OK;
+}
+
+/* */
+int isCutEquivalent(cut_2d_t *a, cut_2d_t *b)
+{
+	if (strcmp(a->path, b->path) == 0 && a->x == b->x && a->y == b->y)
+		return 0;
+	else
+		return 1;
+}
+
+snd_err_t svg_group_end(char *in) { return SND_OK; }
---
+++ test/main.c
@@ -0,0 +1,11 @@
+#include "sndwch.h"
+#include "stdio.h"
+
+int main(){
+
+
+  layer_t l;
+  
+  
+  return 0;
+}
