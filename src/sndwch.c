99bdf44d153ce4122325f989ac96f06a73586746
you <you@example.com>
2017-08-17 23:08:58 +0200

    Improved error handling

--- src/sndwch.c
+++ src/sndwch.c
@@ -13,7 +13,7 @@
  * Parse and validate the resource and free the resulting tree
 makeg */
 
-static void openAndCopyRootNode(const char *filename, xmlNodePtr node_to_copy)
+static snd_err_t openAndCopyRootNode(const char *filename, xmlNodePtr node_to_copy)
 {
 	xmlParserCtxtPtr ctxt; /* the parser context */
 	xmlDocPtr doc;	 /* the resulting document tree */
@@ -21,8 +21,7 @@ static void openAndCopyRootNode(const char *filename, xmlNodePtr node_to_copy)
 	/* create a parser context */
 	ctxt = xmlNewParserCtxt();
 	if (ctxt == NULL) {
-		fprintf(stderr, "Failed to allocate parser context\n");
-		return;
+	  return SWC_ERR_ALLOC;
 	}
 	/* parse the file, activating the DTD validation option */
 	doc = xmlCtxtReadFile(ctxt, filename, NULL, 0);
@@ -37,13 +36,13 @@ static void openAndCopyRootNode(const char *filename, xmlNodePtr node_to_copy)
 		xmlNodePtr rootNode = xmlDocGetRootElement(doc);
 		if (rootNode == NULL) {
 			printf("testXmlwriterTree: Error creating the xml node\n");
-			return;
+			return SWC_ERR_IN_FILE;
 		}
 
 		xmlNodePtr node_copy = xmlCopyNode(rootNode, 1);
 		if (node_copy == NULL) {
 			printf("testXmlwriterTree: Error creating the xml node\n");
-			return;
+			return SWC_ERR_IN_FILE ;
 		}
 		xmlAddChildList(node_to_copy, node_copy->children);
 
@@ -56,219 +55,35 @@ static void openAndCopyRootNode(const char *filename, xmlNodePtr node_to_copy)
 
 snd_err_t swc_merge(const char **in_paths, size_t in_paths_size, const char *out_path)
 {
+	int res;
+	xmlDocPtr doc;
+	xmlNodePtr svgRoot;
 
 	LIBXML_TEST_VERSION
 
-	int rc;
-	xmlNodePtr rootCopy;
-	xmlTextWriterPtr writer;
-	xmlDocPtr doc;
-
-	/* Create a new XML DOM tree, to which the XML document will be
-	 * written */
+	/* creating base document */
 	doc = xmlNewDoc(BAD_CAST XML_DEFAULT_VERSION);
 	if (doc == NULL) {
 		printf("testXmlwriterTree: Error creating the xml document tree\n");
-		return SWC_ERROR;
+		return SWC_ERR_ALLOC;
 	}
+	svgRoot = xmlNewDocNode(doc, NULL, BAD_CAST "svg", NULL);
+	xmlNewProp(svgRoot, BAD_CAST "xmlns", BAD_CAST "http://www.w3.org/2000/svg");
+	xmlNewProp(svgRoot, BAD_CAST "xmlns:xlink", BAD_CAST "http://www.w3.org/1999/xlink");
+	xmlDocSetRootElement(doc, svgRoot);
 
-	/* Create a new XML node, to which the XML document will be
-	 * appended */
-	xmlNodePtr node = xmlNewDocNode(doc, NULL, BAD_CAST "svg", NULL);
-	if (node == NULL) {
-		printf("testXmlwriterTree: Error creating the xml node\n");
-		return SWC_ERROR;
-	}
-		
-        xmlNewProp(node, BAD_CAST "xmlns", BAD_CAST "http://www.w3.org/2000/svg");
-        xmlNewProp(node, BAD_CAST "xmlns:xlink", BAD_CAST "http://www.w3.org/1999/xlink");
-	
-	/* Make ELEMENT the root node of the tree */
-	xmlDocSetRootElement(doc, node);
-
-	/* Create a new XmlWriter for DOM tree, with no compression. */
-	writer = xmlNewTextWriterTree(doc, node, 0);
-	if (writer == NULL) {
-		printf("testXmlwriterTree: Error creating the xml writer\n");
-		return SWC_ERROR;
-	}
-
-	/* Start the document with the xml default for the version,
-	 * encoding ISO 8859-1 and the default for the standalone
-	 * declaration. */
-	//rc = xmlTextWriterStartDocument(writer, NULL, MY_ENCODING, NULL);
-	if (rc < 0) {
-		printf("testXmlwriterTree: Error at xmlTextWriterStartDocument\n");
-		return SWC_ERROR;
-	}
-
-	xmlNodePtr root = xmlDocGetRootElement(doc);
-	if (root == NULL) {
-		printf("testXmlwriterTree: Error creating the xml node\n");
-		return SWC_ERROR;
-	}
-
+	/* add all files' nodes */
 	for (size_t i = 0; i < in_paths_size; ++i) {
-		openAndCopyRootNode(in_paths[i], root);
+		openAndCopyRootNode(in_paths[i], svgRoot);
 	}
 
-	rc = xmlTextWriterEndDocument(writer);
-	if (rc < 0) {
-		printf("testXmlwriterDoc: Error at xmlTextWriterEndDocument\n");
-		return SWC_ERROR;
+	/* save file */
+	res = xmlSaveFileEnc("./merged.svg", doc, MY_ENCODING);
+	if (res == -1) {
+		return SWC_ERR_IO_WRITE;
 	}
-
-	xmlFreeTextWriter(writer);
-	xmlSaveFileEnc("./merged.svg", doc, MY_ENCODING);
-
+	
 	return SWC_OK;
-}
-
-static void exampleFunc(const char *filename)
-{
-	xmlParserCtxtPtr ctxt; /* the parser context */
-	xmlDocPtr doc;	 /* the resulting document tree */
-
-	/* create a parser context */
-	ctxt = xmlNewParserCtxt();
-	if (ctxt == NULL) {
-		fprintf(stderr, "Failed to allocate parser context\n");
-		return;
-	}
-	/* parse the file, activating the DTD validation option */
-	doc = xmlCtxtReadFile(ctxt, filename, NULL, 0);
-	/* check if parsing suceeded */
-	if (doc == NULL) {
-		fprintf(stderr, "Failed to parse %s\n", filename);
-	} else {
-		/* check if validation suceeded */
-		if (ctxt->valid == 0)
-			fprintf(stderr, "Failed to validate %s\n", filename);
-		/* free up the resulting document */
-		xmlFreeDoc(doc);
-	}
-	/* free up the parser context */
-	xmlFreeParserCtxt(ctxt);
-}
-
-static void writeOut(const char *filename)
-{
-	xmlParserCtxtPtr ctxt; /* the parser context */
-	xmlDocPtr doc;	 /* the resulting document tree */
-
-	/* create a parser context */
-	ctxt = xmlNewParserCtxt();
-	if (ctxt == NULL) {
-		fprintf(stderr, "Failed to allocate parser context\n");
-		return;
-	}
-	/* parse the file, activating the DTD validation option */
-	doc = xmlCtxtReadFile(ctxt, filename, NULL, 0);
-	/* check if parsing suceeded */
-	if (doc == NULL) {
-		fprintf(stderr, "Failed to parse %s\n", filename);
-	} else {
-		/* check if validation suceeded */
-		if (ctxt->valid == 0)
-			fprintf(stderr, "Failed to validate %s\n", filename);
-
-		xmlTextWriterPtr writer;
-		xmlNodePtr node;
-
-		xmlDocPtr doc2;
-		int rc;
-
-		/* Create a new XML DOM tree, to which the XML document will be
-		 * written */
-		doc2 = xmlNewDoc(BAD_CAST XML_DEFAULT_VERSION);
-		if (doc == NULL) {
-			printf("testXmlwriterTree: Error creating the xml document tree\n");
-			return;
-		}
-
-		/* Create a new XML node, to which the XML document will be
-		 * appended */
-		node = xmlNewDocNode(doc2, NULL, BAD_CAST "EXAMPLE", NULL);
-		if (node == NULL) {
-			printf("testXmlwriterTree: Error creating the xml node\n");
-			return;
-		}
-
-		/* Make ELEMENT the root node of the tree */
-		xmlDocSetRootElement(doc2, node);
-
-		xmlNodePtr Node1Root = xmlDocGetRootElement(doc);
-		if (Node1Root == NULL) {
-			printf("testXmlwriterTree: Error creating the xml node\n");
-			return;
-		}
-
-		xmlNodePtr Node2Root = xmlDocGetRootElement(doc2);
-		if (Node2Root == NULL) {
-			printf("testXmlwriterTree: Error creating the xml node\n");
-			return;
-		}
-
-		xmlNodePtr new_node = xmlDocCopyNode(Node1Root, doc, 1);
-		if (new_node == NULL) {
-			printf("testXmlwriterTree: Error creating the xml node\n");
-			return;
-		}
-
-		xmlAddChild(Node2Root, new_node);
-
-		/* Create a new XmlWriter for DOM tree, with no compression. */
-		writer = xmlNewTextWriterTree(doc2, node, 0);
-		if (writer == NULL) {
-			printf("testXmlwriterTree: Error creating the xml writer\n");
-			return;
-		}
-
-		/* Create a new XmlWriter for DOM, with no compression. */
-		// writer = xmlNewTextWriterDoc(&doc2, 0);
-		if (writer == NULL) {
-			printf("testXmlwriterDoc: Error creating the xml writer\n");
-			return;
-		}
-
-		/* Start the document with the xml default for the version,
-	     * encoding ISO 8859-1 and the default for the standalone
-	     * declaration. */
-		rc = xmlTextWriterStartDocument(writer, NULL, MY_ENCODING, NULL);
-		if (rc < 0) {
-			printf("testXmlwriterDoc: Error at xmlTextWriterStartDocument\n");
-			return;
-		}
-
-		/* Start an element named "EXAMPLE". Since thist is the first
-	     * element, this will be the root element of the document. */
-		rc = xmlTextWriterStartElement(writer, BAD_CAST "EXAMPLE");
-		if (rc < 0) {
-			printf("testXmlwriterDoc: Error at xmlTextWriterStartElement\n");
-			return;
-		}
-
-		/* Here we could close the elements ORDER and EXAMPLE using the
-	     * function xmlTextWriterEndElement, but since we do not want to
-	     * write any other elements, we simply call xmlTextWriterEndDocument,
-	     * which will do all the work. */
-		rc = xmlTextWriterEndDocument(writer);
-		if (rc < 0) {
-			printf("testXmlwriterDoc: Error at xmlTextWriterEndDocument\n");
-			return;
-		}
-
-		xmlFreeTextWriter(writer);
-
-		xmlSaveFileEnc("./merged.svg", doc2, MY_ENCODING);
-
-		// xmlFreeDoc(doc2);
-
-		/* free up the resulting document */
-		xmlFreeDoc(doc);
-	}
-	/* free up the parser context */
-	xmlFreeParserCtxt(ctxt);
 }
 
 /* reads all the SVG elements of a file into a buffer */
