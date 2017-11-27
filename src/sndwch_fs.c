105b4b0eb30b8db62104b016e6257800c8bbd36a
sammythegoat <sammythegoat@sammythegoats-macbook-pro.home>
2017-11-27 22:22:11 +0100

    now simplifying file read

--- src/sndwch_fs.c
+++ src/sndwch_fs.c
@@ -1,125 +1,9 @@
 #include "sndwch.h"
-#include <errno.h>  /* for definition of errno */
-#include <stdarg.h> /* ISO C variable aruments */
+#include <yaml.h>
 #include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <yaml.h>
-#include <yaml.h>
 
 static const size_t max_line = 4096; /* max line length */
-static const size_t max_files = 200; /* max line length */
-
-/*
-** Print a message and return to caller.
-** Caller specifies "errnoflag".
-*/
-static void err_doit(int errnoflag, int error, const char *fmt, va_list ap)
-{
-	char buf[max_line];
-
-	vsnprintf(buf, max_line - 1, fmt, ap);
-
-	if (errnoflag)
-		snprintf(buf + strlen(buf), max_line - strlen(buf) - 1, ": %s", strerror(error));
-
-	strcat(buf, "\n");
-	fflush(stdout); /* in case stdout and stderr are the same */
-	fputs(buf, stderr);
-	fflush(NULL); /* flushes all stdio output streams */
-}
-
-/*
-** Fatal error unrelated to a system call.
-** Print a message and terminate.
-*/
-void err_quit(const char *fmt, ...)
-{
-	va_list ap;
-	va_start(ap, fmt);
-	err_doit(0, 0, fmt, ap);
-	va_end(ap);
-	exit(1);
-}
-
-/*
-** Fatal error related to a system call.
-** Print a message, dump core, and terminate.
-*/
-void err_dump(const char *fmt, ...)
-{
-	va_list ap;
-
-	va_start(ap, fmt);
-	err_doit(1, errno, fmt, ap);
-	va_end(ap);
-	abort(); /* dump core and terminate */
-	exit(1); /* shouldn′t get here */
-}
-
-void read_params()
-{
-
-	FILE *fh = fopen("./assets/cuts.yaml", "r");
-	yaml_parser_t parser;
-	yaml_token_t token; /* new variable */
-
-	/* Initialize parser */
-	if (!yaml_parser_initialize(&parser))
-		err_quit("Failed to initialize parser!");
-	if (fh == NULL)
-		err_quit("Failed to open file!");
-
-	/* Set input file */
-	yaml_parser_set_input_file(&parser, fh);
-
-	do {
-		yaml_parser_scan(&parser, &token);
-		switch (token.type) {
-		/* Stream start/end */
-		case YAML_STREAM_START_TOKEN:
-			puts("STREAM START");
-			break;
-		case YAML_STREAM_END_TOKEN:
-			puts("STREAM END");
-			break;
-		/* Token types (read before actual token) */
-		case YAML_KEY_TOKEN:
-			printf("(Key token)   ");
-			break;
-		case YAML_VALUE_TOKEN:
-			printf("(Value token) ");
-			break;
-		/* Block delimeters */
-		case YAML_BLOCK_SEQUENCE_START_TOKEN:
-			puts("<b>Start Block (Sequence)</b>");
-			break;
-		case YAML_BLOCK_ENTRY_TOKEN:
-			puts("<b>Start Block (Entry)</b>");
-			break;
-		case YAML_BLOCK_END_TOKEN:
-			puts("<b>End block</b>");
-			break;
-		/* Data */
-		case YAML_BLOCK_MAPPING_START_TOKEN:
-			puts("[Block mapping]");
-			break;
-		case YAML_SCALAR_TOKEN:
-			printf("scalar %s \n", token.data.scalar.value);
-			break;
-		/* Others */
-		default:
-			printf("Got token of type %d\n", token.type);
-		}
-		if (token.type != YAML_STREAM_END_TOKEN)
-			yaml_token_delete(&token);
-	} while (token.type != YAML_STREAM_END_TOKEN);
-	yaml_token_delete(&token);
-
-	/* Cleanup */
-	yaml_parser_delete(&parser);
-	fclose(fh);
-}
+static const size_t max_files = 200; /* max cut_file_t length */
 
 bool parseKeyScalar(yaml_parser_t *parser, const char *keyName)
 {
@@ -236,7 +120,7 @@ bool parseCutFile(yaml_parser_t *parser, cut_file_t *cut)
  * @param cuts_len output, number of cuts found
  * @todo of course. error checking
  */
-void read_cut_files_from_yaml(const char *filePath, cut_file_t ***cuts, int *cuts_found_ptr)
+snd_err_t read_cut_files_from_yaml(const char *filePath, cut_file_t ***cuts, int *cuts_found_ptr)
 {
 
 	*cuts_found_ptr = 0;
@@ -255,10 +139,8 @@ void read_cut_files_from_yaml(const char *filePath, cut_file_t ***cuts, int *cut
 
 	/* Open file and initialize parser */
 	fh = fopen(filePath, "r");
-	if (!yaml_parser_initialize(&parser))
-		err_quit("Failed to initialize parser!");
-	if (fh == NULL)
-		err_quit("Failed to open file!");
+        if (fh == NULL || !yaml_parser_initialize(&parser))
+                return SWC_ERR_IN_FILE;
 	yaml_parser_set_input_file(&parser, fh);
 
 	/* attempt to read a cut_file_t. one per iteration */
@@ -286,46 +168,5 @@ void read_cut_files_from_yaml(const char *filePath, cut_file_t ***cuts, int *cut
 	yaml_token_delete(&token);
 	yaml_parser_delete(&parser);
 	fclose(fh);
-}
-
-/*
- *
- *
- *
- */
-
-int main()
-{
-
-	snd_err_t res = SWC_OK;
-
-	/*------------- Reading parameters -----------------------*/
-	{
-		cut_file_t **cuts;
-		int cuts_len;
-		read_cut_files_from_yaml("./assets/cuts.yaml", &cuts, &cuts_len);
-
-		for (size_t i = 0; i < cuts_len; i++) {
-			cut_file_t *cut_ptr = cuts[i];
-			printf("found: %s - %.1f - %.1f - %.1f - %.1f\n", cut_ptr->path, cut_ptr->x,
-			       cut_ptr->y, cut_ptr->zstart, cut_ptr->zend);
-		}
-	}
-
-	/*------------- merging two files -----------------------*/
-	{
-		const char *p1 = "./assets/a.svg";
-		const char *p1_ = "/opt/garage/sndwch/test/assets/a.svg";
-		const char *p2 = "./assets/b.svg";
-		const char *p2_ = "/opt/garage/sndwch/test/assets/b.svg";
-		const char *paths[2];
-		paths[0] = p1;
-		paths[1] = p2;
-		res = swc_merge(paths, 2, "./merged.svg");
-		if (res != SWC_OK)
-			err_quit("Error Merging.");
-	}
-	/*--------------------------------------------------------*/
-
-	return 0;
+        return SWC_OK;
 }
